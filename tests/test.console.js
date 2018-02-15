3e0fd8539ea7fd19f138d35f32016671506a56ac
Jayden Rasmussen <jayden.rasmussen@gmail.com>
2018-02-15 14:15:35 -0700

    updated to include tests

---
+++ tests/test.console.js
@@ -0,0 +1,27 @@
+'use strict';
+const test = require('ava');
+const l = require('../lib/lib.console');
+test('error prints, no matter the type', t => {
+    l.error('string');
+    l.error({ prop1: 'object' });
+    l.error(['array']);
+    t.pass();
+});
+test('info prints, no matter the type', t => {
+    l.info('string');
+    l.info({ prop1: 'object' });
+    l.info(['array']);
+    t.pass();
+});
+test('warn prints, no matter the type', t => {
+    l.warn('string');
+    l.warn({ prop1: 'object' });
+    l.warn(['array']);
+    t.pass();
+});
+test('success prints, no matter the type', t => {
+    l.success('string');
+    l.success({ prop1: 'object' });
+    l.success(['array']);
+    t.pass();
+});
